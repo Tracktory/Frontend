05caefc257f2d443e53b56426c8348871cade76e
박성훈 <a01089277435@gmail.com>
2026-05-04 17:07:38 +0900

    feat: RootNavigator에 ChatOverlayHost 연결 및 ChatBotPage 구현

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { View } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { OnboardingNavigator } from './OnboardingNavigator';
 import { MainTabNavigator } from './MainTabNavigator';
+import { ChatOverlayHost } from '../components/chat/ChatOverlayHost';
 
 export type RootStackParamList = {
   Onboarding: undefined;
@@ -13,12 +15,16 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 export function RootNavigator() {
   return (
-    <Stack.Navigator
-      initialRouteName="Onboarding"
-      screenOptions={{ headerShown: false }}
-    >
-      <Stack.Screen name="Onboarding" component={OnboardingNavigator} />
-      <Stack.Screen name="Main" component={MainTabNavigator} />
-    </Stack.Navigator>
+    <View style={{ flex: 1 }}>
+      <Stack.Navigator
+        initialRouteName="Onboarding"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Onboarding" component={OnboardingNavigator} />
+        <Stack.Screen name="Main" component={MainTabNavigator} />
+      </Stack.Navigator>
+      {/* Root 레벨 플로팅 챗봇 버튼 + 슬라이드업 오버레이 */}
+      <ChatOverlayHost />
+    </View>
   );
 }
